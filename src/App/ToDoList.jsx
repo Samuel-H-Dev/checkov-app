9658f19e94dcab2ba8ba7df07593c3e88741d782
Samuel Holmes <samuelHdev@outlook.com>
2023-08-16 15:53:26 -0400

    added mark as done and delete item

--- src/App/ToDoList.jsx
+++ src/App/ToDoList.jsx
@@ -14,13 +14,49 @@ export default function ToDoList({todoItems, setTodoItems}){
         }
     },[user])
 
+    function isDone(done, id){
+        const markAsDone = {
+            done: !done,
+            id
+        }
+        console.log(markAsDone)
+        fetch(`https://checkov-api-sh.web.app/tasks/${user.uid}`,{
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",      
+            },
+            body: JSON.stringify(markAsDone),
+          })
+        .then(res => res.json())
+        .then(setTodoItems)
+        .catch(alert)
+        }
+
+    function remove(id){
+        const item = {
+            id
+        }
+        
+        fetch(`https://checkov-api-sh.web.app/tasks/${user.uid}`,{
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",      
+            },
+            body: JSON.stringify(item),
+          })
+        .then(res => res.json())
+        .then(setTodoItems)
+        .catch(alert)
+        }
+    
+
     if(!todoItems) return <h2>Loading...</h2>
     return(
         <section className="container flex flex-col items-center justify-center w-full mx-auto  bg-violet-950 rounded-lg shadow mt-4 ">
             <ul className="flex flex-col divide-y divide w-full">
 
             {todoItems.map(item => (
-                <ToDoListItem key={item.id} item={item} />
+                <ToDoListItem key={item.id} remove={remove} isDone={isDone} item={item} />
                 ))}
             
             </ul>
