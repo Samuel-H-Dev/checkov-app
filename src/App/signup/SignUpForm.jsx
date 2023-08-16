a4c3ee24c4b4f5d9fa3c1ca6c5925d97d7ac7c0f
Samuel Holmes <samuelHdev@outlook.com>
2023-08-16 15:54:43 -0400

    fixes styles on login and sign up form

--- src/App/signup/SignUpForm.jsx
+++ src/App/signup/SignUpForm.jsx
@@ -55,12 +55,12 @@ export default function SignUpForm(){
         <form onSubmit={handleSignUp} className="flex flex-col items-start justify-around min-h-[25vh]">
             <label htmlFor="email" className="flex justify-between w-full">
             <span>Email:</span>
-                <input type="email" name="email" className="rounded-lg border-transparent border border-violet-500 py-2 px-4 bg-zinc-50 text-zinc-700 placeholder-zinc-400 text-base focus:ring-2 focus:ring-violet-600 focus:border-transparent"/>
+                <input type="email" name="email" className="rounded-lg border border-violet-500 py-2 px-4 bg-zinc-50 text-zinc-700 placeholder-zinc-400 text-base focus:ring-2 focus:ring-violet-600 focus:border-transparent"/>
             </label>
            
             <label htmlFor="password" className="flex justify-between w-full">
             <span>Password:</span>
-                <input type="password" name="password" className="rounded-lg border-transparent border border-violet-500 py-2 px-4 bg-zinc-50 text-zinc-700 placeholder-zinc-400 text-base focus:ring-2 focus:ring-violet-600 focus:border-transparent"/>
+                <input type="password" name="password" className="rounded-lg border border-violet-500 py-2 px-4 bg-zinc-50 text-zinc-700 placeholder-zinc-400 text-base focus:ring-2 focus:ring-violet-600 focus:border-transparent"/>
             </label>
            
             <input type="submit" value="Sign up" className="bg-green-300 text-violet-950 py-2 px-8 rounded-lg shadow-lg transition duration-300 hover:bg-green-500 hover:shadow-sm cursor-pointer w-full"/>
